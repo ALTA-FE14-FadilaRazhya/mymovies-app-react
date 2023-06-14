80a8f5fc0ad03ebfdee10a1529b458543a967578
fadilarazhya <132424254+fadilarazhya@users.noreply.github.com>
2023-06-14 15:11:44 +0700

    Add hook to now playing movie

--- mymovies-app-react/src/NowPlaying.tsx
+++ mymovies-app-react/src/NowPlaying.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, {FC, useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from './components/Card';
 import Navbar from './components/Navbar';
@@ -11,21 +11,14 @@ interface Movie {
   poster_path: string;
 }
 
-interface NowPlayingMoviesState {
-  movies: Movie[];
-}
+const NowPlaying: FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-class NowPlaying extends Component<NowPlayingMoviesState> {
-  state: NowPlayingMoviesState = {
-    movies: [],
-  };
+  useEffect(() => {
+    fetchNowPlayingMovies();
+  }, []);
 
-
-  componentDidMount() {
-    this.fetchNowPlayingMovies();
-  }
-
-  fetchNowPlayingMovies = async () => {
+  const fetchNowPlayingMovies = async () => {
     try {
       const response = await axios.get(
         'https://api.themoviedb.org/3/movie/now_playing',
@@ -37,80 +30,32 @@ class NowPlaying extends Component<NowPlayingMoviesState> {
           },
         }
       );
-      this.setState({ movies: response.data.results });
+      setMovies(response.data.results);
     } catch (error) {
       console.error('Error fetching now playing movies:', error);
     }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div>
+  return (
+    <div>
       <Navbar />
-       <div className="m-10 gap-y-5 gap-x-5 flex flex-row">
-        {list ?
-         movies.map((movie) => {
-           return (
-             <Card 
-               key={movie.id}
-               id="list"
-               image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-               title={movie.title}
-               description={movie.overview}
-             />
-           );
-         }) : undefined}
-       </div>
-     </div>
-  )
+      <div className="m-10 gap-y-5 gap-x-5 flex flex-row">
+        {list ? (
+          movies.map((movie) => (
+            <Card
+              key={movie.id}
+              id="list"
+              image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              title={movie.title}
+              description={movie.overview}
+            />
+          ))
+        ) : (
+          undefined
+        )}
+      </div>
+    </div>
+  );
+};
 
-    // return (
-    //   <div>
-    //     <h2>Now Playing Movies</h2>
-    //     <ul>
-    //       {movies.map((movie) => (
-    //         <li key={movie.id}>
-    //           <img
-    //             src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-    //             alt={movie.title}
-    //           />
-    //           <span>{movie.title}</span>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    // );
-  }
-}
-
-// class NowPlaying extends Component {
-//   render() {
-//     return (
-//       <div>
-//       {/* Other components */}
-//       <NowPlayingMovies />
-//       </div>
-
-//       //   <div>
-//       //   <Navbar />
-//       //    <div className="m-10 gap-y-5 gap-x-5 flex flex-row">
-//       //     {list ?
-//       //      list.map((item, index) => {
-//       //        return (
-//       //          <Card 
-//       //            key={index}
-//       //            id="list"
-//       //            image={item.image}
-//       //            title={item.title}
-//       //            description={item.description}
-//       //          />
-//       //        );
-//       //      }) : undefined}
-//       //    </div>
-//       //  </div>
-//     )
-//   }
-// }
 export default NowPlaying;
